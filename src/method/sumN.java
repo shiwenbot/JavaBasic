75e45c70094fde47623cc8c89ac39a5f7c2ead88
Shiwen <shiwenbot@gmail.com>
2022-06-04 14:41:55 +0100

    update

--- src/method/sumN.java
+++ src/method/sumN.java
@@ -2,6 +2,15 @@ package method;
 
 public class sumN {
     public static void main(String[] args) {
-        System.out.println("2");
+        //return the summation for numbers from 1 to n
+        System.out.println(sum(10));
+    }
+
+    public static int sum(int n){
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
